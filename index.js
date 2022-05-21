770218b896d8cd7cf5767f105f6e704c64b87b43
yaserfarook1 <98206606+yaserfarook1@users.noreply.github.com>
2022-05-21 06:15:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+document.querySelector(".cartbtn").addEventListener("click",getVal());
+function getVal() {
+const val = document.querySelector("#quantity"). value;
+alert("The total number of quantity is " +val);
+}
