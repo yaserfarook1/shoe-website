3cfa0ceedd7f6e8a9b51c8825b8822b819c466b3
yaserfarook1 <98206606+yaserfarook1@users.noreply.github.com>
2022-05-31 05:50:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 
 
-$.alert("okok");
 
+$.alert("okok");
 
 document.querySelector(".cartbtn").addEventListener("click",getVal());
 function getVal() {
 const val = document.querySelector("#quantity"). value;
 alert("The total number of quantity is " +val);
 }
+
+
+const list = document.querySelector('.allshoe'),
+items = Array.from(list.querySelectorAll('.shoesort'));
+
+console.log(items.sort((a, b) => a - b));
+
+
+items.forEach(el => list.append(el))
